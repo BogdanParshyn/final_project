592b6210b747056b6cc9d3e981185dd9a20e90aa
Module <bogdan.alexsandrovich@gmail.com>
2019-08-04 16:17:22 +0300

    1.1

--- table.jsx
+++ table.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./style.css"
+var report_table=[];
 function isObject(obj) {
 		return obj === Object(obj);
 	}
@@ -7,7 +8,8 @@ export default class Table extends React.Component{
 	constructor(){
 		super();
 		this.state={
-			report_table:[]
+			report_table:[],
+			data:[]
 		}
 		this.buildTable=this.buildTable.bind(this)
 	}
@@ -17,23 +19,33 @@ export default class Table extends React.Component{
 		}
 	}
 	buildTable(){
+		report_table=[];
+		this.setState({
+			report_table:[],
+			data:[]
+		});
 		fetch(this.props.choiceAPI,{method: 'GET'})
 					.then(res=>res.json())
 					.then(res=>{
+						report_table.push(this.props.boxes_request)
 						for(var key in res) {
 							var value = res[key];
 							if(isObject(value)==true){
 								for(var key in value) {
 									var result = value[key];
-										var data = [
-											this.props.boxes_request,
-											this.props.boxes_request.map(key => value[key])
-										]
-									console.log(data)
+									this.setState({
+										data: this.props.boxes_request.map(key => value[key])
+									});
 								}
+								
+								report_table.push(this.state.data)
+								
 							}
+						
 						}
 					});
+					console.log(report_table)
+					
 	}
 	
 	render(){
