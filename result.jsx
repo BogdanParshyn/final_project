cbaa6547fab9a987005d9662a93d88783682c059
mihail <mihailpaslevel@gmail.com>
2019-08-08 13:28:10 +0300

    commit

--- result.jsx
+++ result.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import "./style.css"
-import xlsx from 'node-xlsx';
 export default class Result extends React.Component{
 	constructor(){
 		super();
 		this.onCreateTable=this.onCreateTable.bind(this)
 	}
 	onCreateTable(){
-		const data = [this.props.boxes_request];
-				var buffer = xlsx.build([{name: "mySheetName", data: data}]);
-		console.log(buffer)
+		
 	}
 	render(){
 		return(
