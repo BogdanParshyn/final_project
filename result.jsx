ee892206ffc47effb1ba92042bed0167d2f02110
mihail <mihailpaslevel@gmail.com>
2019-08-10 12:52:47 +0300

    commit

--- result.jsx
+++ result.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import "./style.css"
 import ReactHTMLTableToExcel from 'react-html-table-to-excel';
+var today = new Date();
 export default class Result extends React.Component{
-	constructor(){
-		super();
-		
-	}
 	
 	render(){
 		return(
 			<div className="result_block">
 				<ReactHTMLTableToExcel
                     id="test-table-xls-button"
-                    className="download-table-xls-button"
+                    className="download_table"
                     table="table-to-xls"
-                    filename="tablexls"
-                    sheet="tablexls"
-                    buttonText="Download as XLS"/>
-               
+                    filename="report"
+                    sheet="table"
+                    buttonText="Download"/>
 			</div>
 		)
 	}
